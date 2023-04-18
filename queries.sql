7d8e8b89c2088771c9833c167eda92c9b30fd4e3
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2023-04-18 00:59:55 +0000

    update-2159

--- queries.sql
+++ queries.sql
@@ -52,7 +52,7 @@ WHERE InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
 
 
 
-
+/* Get a list of customers,sales rep, total transactions for each cust 2011-2012  */
 
 SELECT 
 	c.FirstName AS [Customer FN],
@@ -74,3 +74,28 @@ ORDER BY
 	i.total DESC
 
 
+
+/* how many transactions are above the avg amount during the same time? */
+
+SELECT
+	round(avg(total),2) AS [Avg Transaction Total]
+FROM
+	Invoice
+WHERE
+	InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
+
+/* above average */
+
+SELECT 
+	count(Total) AS [Num of Transactions above avg]
+FROM
+	Invoice
+WHERE
+	Total >
+( SELECT 
+	round(avg(total),2) AS [AVG TRANSACTION TOTAL]
+FROM
+	Invoice
+WHERE
+	InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
+)
